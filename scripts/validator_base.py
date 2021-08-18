8487167d2682660090a45c609a26684f3db8351f
Keenan Gugeler <me@kgugeler.ca>
2021-08-18 15:11:24 -0400

    allow lack of validator

--- scripts/validator_base.py
+++ scripts/validator_base.py
@@ -2,10 +2,20 @@ from .executors import Executor
 
 class Validator:
     def __init__(self, file):
-        self.validator = Executor(file)
+        if not file:
+            print("Warning! No validator. Proceeding anyway.")
+
+            self.validator = None
+
+        else:
+            self.validator = Executor(file)
 
     def validate(self, input):
-        return self.validator.run(input = input, check_success = False).returncode == 0
+        if self.validator is None:
+            return True
+
+        else:
+            return self.validator.run(input = input, check_success = False).returncode == 0
 
     def assertValid(self, input):
         assert self.validate(input), "Invalid input!"
