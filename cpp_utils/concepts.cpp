805b25b0ef3f905fc3ebfb60300d36201a8518db
Keenan Gugeler <me@kgugeler.ca>
2021-08-31 00:07:25 -0400

    readInts to reader

--- cpp_utils/concepts.cpp
+++ cpp_utils/concepts.cpp
@@ -8,3 +8,8 @@ template<typename F, typename T>
 concept Predicate = requires(F f, T x) {
   { f(x) } -> convertible_to<bool>;
 };
+
+template<typename F, typename T>
+concept Monad = requires(F f, T t) {
+  f(t);
+};
