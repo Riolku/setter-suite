3e8201e5cc4c8730be62041f75e4a7d927aa91f0
Keenan Gugeler <me@kgugeler.ca>
2022-01-03 19:42:00 -0500

    use proper error codes in checker utils

--- cpp_utils/checker_utils.cpp
+++ cpp_utils/checker_utils.cpp
@@ -11,7 +11,8 @@ class CheckerReader : public Reader::FileReader {
 
     void __attribute__((noreturn)) errorHandler(enum Reader::error_type e) {
         switch (e) {
-        case Reader::error_type::OUT_OF_RANGE:
+        case Reader::error_type::INTERNAL_RANGE:
+        case Reader::error_type::EXTERNAL_RANGE:
             exit(CheckerCodes::WA);
 
         case Reader::error_type::INVALID_ARGUMENT:
