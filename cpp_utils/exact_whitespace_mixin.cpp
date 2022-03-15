a269669c4a1243f50c369b9a35abbc90bb214d55
Keenan Gugeler <me@kgugeler.ca>
2022-03-14 23:49:34 -0400

    correct whitespace mixin functions

--- cpp_utils/exact_whitespace_mixin.cpp
+++ cpp_utils/exact_whitespace_mixin.cpp
@@ -1,20 +1,19 @@
 template <typename Parent> class ExactWhitespaceMixin : public Parent {
-  protected:
+  public:
     void readNewLine() override {
         if (Parent::readChar() != '\n')
             Parent::wrongWhitespaceError();
     }
 
     void readEOF() override {
-        if (Parent::eof())
-            Parent::wrongWhitespaceError();
-    }
-
-    void readSpace() override {
         if (!Parent::eof())
             Parent::wrongWhitespaceError();
     }
 
-  public:
+    void readSpace() override {
+        if (Parent::readChar() != ' ')
+            Parent::wrongWhitespaceError();
+    }
+
     using Parent::Parent;
 };
