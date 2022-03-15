8f39be69c619b50f9d98ff426507e16b8b35daad
Keenan Gugeler <me@kgugeler.ca>
2022-03-15 12:22:42 -0400

    Add `list` and `integer_range` in a finished state.

--- cpp_utils/integer_range.cpp
+++ cpp_utils/integer_range.cpp
@@ -1,15 +1,49 @@
-template <typename T = ll> class RangeBase {
+template <typename T = ll> class Range {
     T l, r;
 
-  public:
-    using value_type = T;
-    RangeBase(T l, T r) : l(l), r(r) {}
+    class iterator {
+        T cur;
 
-    T operator[](T offset) const {
-        assert(l + offset < r);
-        return l + offset;
+      public:
+        using difference_type = void;
+        using value_type = T;
+        using pointer = void;
+        using reference = void;
+        using iterator_category = bidirectional_iterator_tag;
+
+        iterator(T cur) : cur(move(cur)) {}
+
+        iterator &operator++() {
+            ++cur;
+            return *this;
+        }
+        iterator &operator--() {
+            --cur;
+            return *this;
+        }
+        T operator*() { return cur; }
+
+        bool operator<(const iterator &other) const { return cur < other.cur; }
+        bool operator!=(const iterator &other) const { return cur != other.cur; }
+    };
+
+  public:
+    Range(T l, T r) : l(move(l)), r(move(r)) {}
+    Range(T r) : l(T()), r(move(r)) {}
+
+    template <typename F> bool any_of(F f) const { return ::any_of(all(*this), f); }
+    template <typename F> bool all_of(F f) const { return ::any_of(all(*this), f); }
+
+    template <typename F> void for_each(F f) const { ::for_each(all(*this), f); }
+
+    template <typename F> auto map_new(F f) const {
+        List<decltype(declval<F>()(declval<T>()))> ret;
+        ret.reserve(this->size());
+        ::transform(all(*this), back_inserter(ret), f);
+        return ret;
     }
 
-    T begin() const { return l; }
-    T end() const { return r; }
+    auto size() const { return r - l; }
+    T begin() const { return iterator(l); }
+    T end() const { return iterator(r); }
 };
--- cpp_utils/utils.cpp
+++ cpp_utils/utils.cpp
@@ -3,3 +3,8 @@ typedef unsigned long long ull;
 typedef long double ld;
 
 #define all(x) (x).begin(), (x).end()
+
+template <typename P> auto to_pair(P p) {
+    auto [a, b] = p;
+    return make_pair(move(a), move(b));
+}
