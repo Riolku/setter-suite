562b8225aefaadadb8047fda97e3ec469485514a
Keenan Gugeler <me@kgugeler.ca>
2022-03-15 21:59:30 -0400

    delete tests
    maybe I'll bring it back when they are actually tested

--- tests/random_arr.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-
-N = 10
-
-print(*[randint(1, 10) for _ in range(N)])
--- tests/random_rte.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-arr = sorted(map(int, input().split()))
-
-if randint(1, 500) == 1:
-    raise RuntimeError
-else:
-    print(*arr)
