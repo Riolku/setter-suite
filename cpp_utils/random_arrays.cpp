5b98d6f3f7b4a8d94a2fb26a53b8f4eb62bc81f6
Keenan Gugeler <me@kgugeler.ca>
2022-03-18 00:42:45 -0400

    fix `with_gaps` random_array function

--- cpp_utils/random_arrays.cpp
+++ cpp_utils/random_arrays.cpp
@@ -8,11 +8,11 @@ vector<ll> random_array(int N, ll lo, ll hi) {
 void shuffle(vector<ll> &arr) { shuffle(all(arr), get_engine()); }
 
 vector<ll> with_gaps(int N, ll lo, ll hi, ll gap) {
-    vector<ll> ret = random_array(N, lo, hi - gap * N);
+    vector<ll> ret = random_array(N, lo, hi - gap * (N - 1));
     sort(all(ret));
-    int i = -1;
-    transform(all(ret), ret.begin(), [&i](ll x) {
-        ++i;
+    int i = 0;
+    transform(ret.begin() + 1, ret.end(), ret.begin(), [&i](ll x) {
+        i += gap;
         return x + i;
     });
     shuffle(ret);
