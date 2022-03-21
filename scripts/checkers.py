af7df043bffad9b37a42c3c95a0dd56cd88ccf34
Keenan Gugeler <me@kgugeler.ca>
2022-03-21 12:35:38 -0400

    implement standard checker

--- scripts/checkers.py
+++ scripts/checkers.py
@@ -1,5 +1,7 @@
 import multiprocessing, os
 
+from typing import Tuple
+
 from .executors import Executor
 
 codes = dict(AC=0, WA=1, PE=2, IE=3, PARTIAL=7, TLE=1000, RTE=2000)
@@ -28,18 +30,74 @@ class BuiltinChecker:
         if id == "identical":
             return IdenticalChecker()
 
+        if id == "standard":
+            return StandardChecker()
+
         raise NotImplementedError(f"no such checker '{id}'")
 
 
-class IdenticalChecker:
-    def __init__(self):
-        pass
-
+class IdenticalChecker(BuiltinChecker):
     def check(self, inp, out, expected_out):
         if out == expected_out:
             return codes["AC"]
 
         return codes["WA"]
+
+
+def isline(c: str) -> bool:
+    return c in "\n\r"
+
+
+def iswhite(c: str) -> bool:
+    return c in " \t\v\f\n\r"
+
+
+class StandardChecker:
+    def _skip_spaces(self, s: str, pos: int) -> Tuple[int, int]:
+        saw_line = saw_space = 0
+        while pos < len(s):
+            saw_line |= isline(s[pos])
+            if not iswhite(s[pos]):
+                break
+            pos += 1
+            saw_space = 1
+
+        return (2 if saw_line else saw_space, pos)
+
+    def _check(self, inp: str, process: str, judge: str) -> bool:
+        j = 0
+        p = 0
+        while j < len(judge) and iswhite(judge[j]):
+            j += 1
+
+        while p < len(process) and iswhite(process[p]):
+            p += 1
+
+        while True:
+            js, j = self._skip_spaces(judge, j)
+            ps, p = self._skip_spaces(process, p)
+
+            if j == len(judge) or p == len(process):
+                return j == len(judge) and p == len(process)
+
+            if js != ps:
+                return False
+
+            while j < len(judge) and not iswhite(judge[j]):
+                if p >= len(process):
+                    return False
+
+                if judge[j] != process[p]:
+                    return 0
+
+                p += 1
+                j += 1
+
+    def check(self, inp: str, process: str, judge: str) -> int:
+        if self._check(inp, process, judge):
+            return codes["AC"]
+        else:
+            return codes["WA"]
 
 
 class CustomChecker:
