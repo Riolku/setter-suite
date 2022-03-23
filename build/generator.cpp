00b3a19306e29544d95bbbde832b85f7dae1932f
Keenan Gugeler <me@kgugeler.ca>
2022-03-23 14:20:13 -0400

    add helper for case counts

--- build/generator.cpp
+++ build/generator.cpp
@@ -299,12 +299,18 @@ public:
 // clang-format off
 List<List<shared_ptr<Test>, 1>, 1> cases = {
   {
-    
+
   }
 };
 // clang-format on
 
 int main(int argc, char **argv) {
+  if (argv[1] == string("case-counts")) {
+    print(cases.map_new(
+        [](const List<shared_ptr<Test>, 1> &suite) { return suite.size(); }));
+    return 0;
+  }
+
   assert(argc >= 3);
 
   int suite = atoi(argv[1]);
