1cd51535c283b972fa9dcbba28e2827c3def7a82
Keenan Gugeler <me@kgugeler.ca>
2022-03-24 16:03:13 -0400

    add pairs to generator dependencies

--- build/generator.cpp
+++ build/generator.cpp
@@ -298,6 +298,42 @@ template <typename T> vector<T> array_with_sum(int N, T sum, T lo) {
   return ret;
 }
 
+
+namespace UnorderedPair {
+// starts counting from zero
+// unordered pair x < y
+
+// Given a pair, get the integer
+ll to_num(ll x, ll y) { return y * (y - 1) / 2 + y - x - 1; }
+
+// Given an integer, get a pair
+pair<ll, ll> to_pair(ll v) {
+  ll y = ll((1 + sqrt(1 + 8 * v)) / 2);
+
+  return {y * (y - 1) / 2 + y - v - 1, y};
+}
+}; // namespace UnorderedPair
+
+namespace OrderedPair {
+// x != y, zero-indexed
+ll to_num(ll x, ll y) {
+  if (x > y) {
+    return x * (x - 1) + y;
+  } else {
+    return y * y + x;
+  }
+}
+
+pair<ll, ll> to_pair(ll v) {
+  ll x = ll(1 + sqrt(1 + 4 * v) / 2);
+  if (x * x <= v) {
+    return {v - x * x, x};
+  } else {
+    return {x, v - x * (x - 1)};
+  }
+}
+}; // namespace OrderedPair
+
 struct Test {
   virtual void generate() = 0;
 
