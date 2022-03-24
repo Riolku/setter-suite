49ef910edfbbbecb693a73c467898f0786a75515
Keenan Gugeler <me@kgugeler.ca>
2022-03-24 13:51:58 -0400

    allow aliases for case counts

--- cpp_utils/gen_entry.cpp
+++ cpp_utils/gen_entry.cpp
@@ -5,9 +5,10 @@ List<List<shared_ptr<Test>, 1>, 1> cases = {
   }
 };
 // clang-format on
+vector<string> case_count_flags = {"case-counts", "cc", "case_counts"};
 
 int main(int argc, char **argv) {
-  if (argv[1] == string("case-counts")) {
+  if (find(all(case_count_flags), argv[1]) != case_count_flags.end()) {
     print(cases.map_new(
         [](const List<shared_ptr<Test>, 1> &suite) { return suite.size(); }));
     return 0;
