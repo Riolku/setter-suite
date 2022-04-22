f2fe4d15d8003cea8c5ef10abaed97c78b48d350
Keenan Gugeler <me@kgugeler.ca>
2022-04-21 21:44:14 -0400

    add compile script

--- scripts/compile.py
+++ scripts/compile.py
@@ -3,5 +3,5 @@ from .executors import Executor
 
 def main(args):
     assert len(args) > 0, "Must provide the file to compile!"
-    e = Executor(args[0])
+    e = Executor(args[0], debug="debug" in args, force_compile="force" in args)
     return 0
