016a17919d53e383a55bcfa7705cf953d678f90e
Keenan Gugeler <me@kgugeler.ca>
2022-06-29 00:40:08 -0400

    push some backed up changes

--- cpp_utils/validation.cpp
+++ cpp_utils/validation.cpp
@@ -1,18 +1,7 @@
-struct IntegerValidation {
-  ll lo, hi;
-
-  bool check(ll x) { return lo <= x && x <= hi; }
-  template <typename R> bool check(const R &r) {
-    for (auto x : r)
-      if (!check(x))
-        return false;
-    return true;
-  }
-
-  template <typename T, typename... Ts> bool check(const T &arg, Ts &&...args) {
-    return check(arg) && check(args...);
-  }
-};
+void assert_permutation(const List<int, 1> &arr) {
+  List<int, 1> check(arr.size(), 0);
+  assert(arr.all_of([&check](int x) { return ++check[x] == 1; }));
+}
 
 class ValidatingReaderBase : public BaseReader {
 protected:
