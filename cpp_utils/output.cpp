10ca2cca2879bf0db5417a69ca589c44378a024a
Keenan Gugeler <me@kgugeler.ca>
2022-08-08 21:17:37 -0400

    fix printer

--- cpp_utils/output.cpp
+++ cpp_utils/output.cpp
@@ -3,6 +3,7 @@ FILE *stream = stdout;
 
 void set_stream(FILE *st) { stream = st; }
 
+// simple types
 void print_impl(bool x) { fprintf(stream, "%d", x); }
 void print_impl(int x) { fprintf(stream, "%d", x); }
 void print_impl(ll x) { fprintf(stream, "%lld", x); }
@@ -10,6 +11,16 @@ void print_impl(size_t x) { fprintf(stream, "%lu", x); }
 void print_impl(const char *x) { fprintf(stream, "%s", x); }
 void print_impl(const string &x) { print_impl(x.c_str()); }
 void print_impl(char x) { fprintf(stream, "%c", x); }
+
+// Recursive types. First, declare all overloads
+template <typename T> void print_impl(const T &);
+template <typename A, typename B> void print_impl(const pair<A, B> &);
+template <typename... Ts> void print_impl(const tuple<Ts...> &t);
+template <typename T> void print_impl(const set<T> &arr);
+template <typename T> void print_impl(const vector<T> &arr);
+template <typename T> void print_impl(const unordered_set<T> &arr);
+
+// Recursive type definitions
 template <typename T> void print_impl(const T &x) {
   print_impl(x.get_printable());
 }
@@ -62,10 +73,8 @@ template <typename T> void print_impl(const unordered_set<T> &arr) {
   print_iterable(arr);
 }
 
-void print_many() {}
-
+// print_many helper
 template <typename T> void print_many(const T &arg) { print_impl(arg); }
-
 template <typename T, typename... Ts>
 void print_many(const T &arg, Ts &&...args) {
   print_impl(arg);
@@ -73,13 +82,12 @@ void print_many(const T &arg, Ts &&...args) {
   print_many(forward<Ts>(args)...);
 }
 
-void print() { fprintf(stream, "\n"); }
-
 template <typename... Ts> void print(Ts &&...args) {
   print_many(forward<Ts>(args)...);
 
   print();
 }
+template <> void print() { fprintf(stream, "\n"); }
 
 template <typename R> void print_items(const R &r) {
   for (auto x : r) {
@@ -87,12 +95,11 @@ template <typename R> void print_items(const R &r) {
   }
 }
 
-void print_yes_no(bool x) { print(x ? "YES" : "NO"); }
-
-template <typename T> void print_yes_no(vector<T> xs) {
-  for (const T &x : xs)
+template <typename T> void print_yes_no(const T &xs) {
+  for (const auto &x : xs)
     print_yes_no(x);
 }
+template <> void print_yes_no(const bool &x) { print(x ? "YES" : "NO"); }
 }; // namespace Printer
 
 using Printer::print;
