d0101e2e81071a0444d6214dc1c1904a3c7930cf
Keenan Gugeler <me@kgugeler.ca>
2022-09-29 08:37:49 -0400

    add features

--- rust_utils/src/lib.rs
+++ rust_utils/src/lib.rs
@@ -1,11 +1,23 @@
+#[cfg(feature = "checker")]
 pub mod checker;
+
+#[cfg(feature = "reader")]
 pub mod reader;
 
+#[cfg(feature = "identical_checker")]
 pub mod identical_checker;
+
+#[cfg(feature = "identical_whitespace")]
 pub mod identical_whitespace;
 
+#[cfg(feature = "standard_checker")]
 pub mod standard_checker;
+
+#[cfg(feature = "standard_whitespace")]
 pub mod standard_whitespace;
 
+#[cfg(feature = "panic_error_handler")]
 pub mod panic_error_handler;
+
+#[cfg(feature = "validating_reader")]
 pub mod validating_reader;
